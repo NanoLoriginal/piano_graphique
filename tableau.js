4e8efc10565a11caef9ea68b9681f62bd81bda38
Kyliann <92451561+NanoLoriginal@users.noreply.github.com>
2021-12-19 14:07:13 +0100

    Update tableau.js

---
+++ tableau.js
@@ -0,0 +1,43 @@
+
+
+class tableau extends Phaser.Scene {
+
+
+    /**
+     * Précharge les assets
+     */
+    preload() {
+
+    }
+    create(){
+
+    }
+    initKeyboard(){
+        let me=this;
+        this.input.keyboard.on('keydown', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.RIGHT:
+                    me.speed=5;
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.LEFT:
+                    me.speed=-5;
+                    break;
+            }
+        });
+        this.input.keyboard.on('keyup', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.RIGHT:
+                case Phaser.Input.Keyboard.KeyCodes.LEFT:
+                    me.speed=0;
+                    break;
+            }
+        });
+    }
+    update(){
+
+    }
+}
